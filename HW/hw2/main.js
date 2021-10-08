9814b6c0b22067a3e8f99ca643f4bbda1022bb3e
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2021-10-08 15:28:12 +0530

    Added labels, axis for First graph

--- HW/hw2/main.js
+++ HW/hw2/main.js
@@ -8,7 +8,7 @@ let gap_between_views = 150;
 let newMargin = {left: 150, right:150, top:150, bottom:150};
 
 
-
+// FIRST GRAPH
 
 const width = 800;
 const height = 900;
@@ -34,17 +34,52 @@ d3.csv("citi_bike_2020.csv").then(function(data) {
 
     // X Axis and Scale
 let xScale = d3.scaleLinear()
-        .range([0, width - (newMargin.left + newMargin.right)])
-        .domain([0, d3.max(data, (d) => d.tripdurationS)]);
+    .range([0, width - (newMargin.left + newMargin.right)])
+    .domain([0, d3.max(data, (d) => d.tripdurationS)]);
 let xAxis = d3.axisBottom(xScale)
-
 firstGraph.append('g')
-    .attr("transform", "translate(0, " + (newMargin.right+50) + ")")
+    .attr("transform", "translate(0, " + (newMargin.left+100) + ")")
     .attr('class', 'x-axis')
     .call(xAxis)
 
+// Y Scale
+let yScale = d3.scaleLinear()
+    .range([height - (newMargin.left + newMargin.right), 0])
+    .domain([0, d3.max(data, (d) => d.tripdurationE)]);
     // G does not have width and height
-    
+let yAxis = d3.axisLeft(yScale)
+    .ticks(500)
+firstGraph.append('g')
+    // .attr("transform", "translate(0, " + (newMargin.left+100) + ")")
+    .attr('class', 'y-axis')
+    .call(yAxis)
+
+
+// Now we add axis labels
+
+// Adding the scatter plots
+
+
+// Interaction
+
+
+
+
+
+// SECOND GRAPH
+
+
+
+
+
+
+
+
+
+
+
+
+
 let secondGraph = d3.select("svg").append("g")
 
     // Need Only the Month of May    
